189cae9ff56ba7738a928db14dc528a8ac88f76f
Chris McLeod <chris@chrismcleod.dev>
2024-05-26 16:53:01 +0100

    chore: refactoring

--- src/utils/fileHandler.mjs
+++ src/utils/fileHandler.mjs
@@ -1,25 +1,31 @@
 import { promises as fs } from "fs";
 import path from "path";
+import { v4 as uuid } from "uuid";
 
-export async function saveImage(imageFile, originalFileName) {
-  const saveDir = path.join(process.cwd(), process.env.SAVE_PATH || "saved/");
-
-  // Generate a unique filename for the image
-  const filename = generateUniqueFilename(originalFileName);
-
-  // Save the image from the temporary directory
-  const imagePath = path.join(saveDir, filename);
-  await fs.copyFile(imageFile.path, imagePath);
-
-  return imagePath;
-}
+const saveDir = path.join(process.cwd(), process.env.SAVE_PATH || "saved/");
 
 function generateUniqueFilename(originalFileName) {
   // Get the file extension from the original file name
   const fileExtension = path.extname(originalFileName);
 
-  // Generate a unique filename using a timestamp and a random number
-  const timestamp = Date.now();
-  const randomNumber = Math.floor(Math.random() * 10000);
-  return `${timestamp}-${randomNumber}${fileExtension}`;
+  // Generate a unique filename using uuid
+  const uniqueName = uuid();
+  return `${uniqueName}${fileExtension}`;
 }
+
+export async function saveImage(imageFile, originalFileName) {
+  try {
+    const filename = generateUniqueFilename(originalFileName);
+
+    // Save the image from the temporary directory
+    const imagePath = path.join(saveDir, filename);
+    await fs.copyFile(imageFile.path, imagePath);
+
+    return imagePath;
+  } catch (error) {
+    console.error(`Failed to save image: ${error}`);
+    throw error; // re-throw the error so it can be handled by the caller
+  }
+}
+
+
